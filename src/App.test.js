7ad01c9ffb867ef741228daa058475480356d1bd
Aviwe Ngqukumba <aviwe3226@gmail.com>
2023-01-10 19:39:39 +0200

    Tets setup and implementation

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App tests', () => {
+  it('Should contain the heading', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Fill in your card details/i);
+    expect(linkElement).toBeInTheDocument();
+
+  });
+
 });
